a6b74619e0eb4e165356030bb8f3f6163e2ca582
Nil Polack <npolack@3E7.42angouleme.fr>
2025-01-20 18:27:56 +0100

    boulot perso sur branch nil (exec)

--- srcs/binary_tree.c
+++ srcs/binary_tree.c
@@ -6,7 +6,7 @@
 /*   By: npolack <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 17:29:00 by npolack           #+#    #+#             */
-/*   Updated: 2025/01/20 16:23:49 by npolack          ###   ########.fr       */
+/*   Updated: 2025/01/20 17:41:11 by npolack          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,66 +25,19 @@ t_bintree	*make_node(t_bintree *left, t_bintree *right ,t_token *token)
 	return (root);
 }
 
-//echo world > file && cat file | echo FAR >> file && cat file | grep FAR
-/*
-t_bintree	*make_little_tree(t_token *head, t_bintree *root)
-{
-	t_bintree	*current_leaf;
-	t_bintree	*new_root;
-	t_bintree	*old_root;
-	t_token		*current_token;
-
-	current_token = head;
-	while (current_token)
-	{
-		current_leaf = make_node(NULL, NULL, current_token);
-		if (current_token->type == CMD)
-		{
-			new_root = current_leaf;
-			old_root = new_root;
-		}
-		else if (current_token->type == OPERATOR)
-		{
-			new_root = current_leaf;
-			new_root->left = old_root;
-			old_root = new_root;
-			current_token = make_little_tree(current_token->next, new_root->right);
-			current_token = current_token->next;
-
-		}
-		else if (current_token->type == PIPE)
-		{
-			current_leaf->left = old_root;
-			current = make_little_tree(current_token->next, current_leaf);
-		}
-		else if (current_token
-
-
-		token =  token->next;
-	}
-}
-
-*/
-//echo world > file && cat file | echo FAR >> file && cat file | grep FAR
-// C || C | C | C && C || C
-// build the binary tree from t_token *list (DO NOT FREE the t_token content)
-
 t_bintree	*build_tree(t_token **head, t_type type)
 {
 	t_token		*current_token;
 	t_bintree	*new_root;
 	t_bintree	*old_root;
+	t_type		current_type;
 
 	current_token = *head;
 	if (!current_token)
 		return (NULL);
 	old_root = NULL;
-	printf("Build tree from token %s\n", current_token->content[0]);
 	while (current_token)
 	{
-		printf("current_token = ");
-		print_args(current_token->content);
-		printf("\n");
 		if (!ft_strcmp(current_token->content[0], "("))
 		{
 			current_token = current_token->next;
@@ -96,32 +49,13 @@ t_bintree	*build_tree(t_token **head, t_type type)
 			*head = current_token;
 			break ;
 		}
-		if (type != CMD) // || !ft_strcmp(current_token->content[0], ")"))
+		if (type != CMD)
 		{
-			if (current_token->type != CMD)// ||  !ft_strcmp(current_token->content[0], ")"))
+			if (current_token->type != CMD)
 			{
 				*head = current_token;
 				break ;
 			}
-//			if (current_token->type == PIPE)
-//			{
-//				if (type == PIPE || type == OPERATOR)
-//				{
-//					printf("exit from the tree->>%s\n", new_root->content[0]);
-//					*head = current_token;
-//					break ;
-//				} 
-//			}
-//			if (current_token->type == OPERATOR)
-//			{
-//				if (!ft_strcmp(current_token->content[0], ")") || type == OPERATOR || type == PIPE)
-//				{
-//					printf("exit from the tree->>%s\n", new_root->content[0]);
-//					*head = current_token;
-//					break ;
-//				} 
-//			}
-
 		}
 		if (current_token->type == CMD)
 		{
@@ -129,44 +63,18 @@ t_bintree	*build_tree(t_token **head, t_type type)
 			old_root = new_root;
 			current_token = current_token->next;
 		}
-		else if (current_token->type == OPERATOR)
+		else
 		{
-			if (!ft_strcmp(current_token->content[0], "("))
-			{
-				printf("PARENTHESE !!\n");
-				current_token = current_token->next; // add this line in the if statement
-				type = CMD;	
-			}
-			else
-			{
+			current_type = current_token->type;
 			new_root = make_node(NULL, NULL, current_token);
 			new_root->left = old_root;
-			printf("\"%s\"->left : %s\n",new_root->content[0], new_root->left->content[0]);
 			current_token = current_token->next;
-			printf("Attempt : \"%s\"->right :%s\n", new_root->content[0], current_token->content[0]);
-			new_root->right = build_tree(&current_token, OPERATOR); // 2	
-			printf("DONE : \"%s\"->right :%s\n", new_root->content[0], new_root->right->content[0]);
-			if (!current_token->next)
-				return (new_root);
-			old_root = new_root;
-			}
-		}
-		else if (current_token->type == PIPE)
-		{
-			new_root = make_node(NULL, NULL, current_token);
-			new_root->left = old_root;
-			printf("\"%s\"->left : %s\n",new_root->content[0], new_root->left->content[0]);
-			current_token = current_token->next;
-			printf("Attempt : \"%s\"->right :%s\n", new_root->content[0], current_token->content[0]);
-			new_root->right = build_tree(&current_token, PIPE); // 2	
-			printf("DONE : \"%s\"->right :%s\n",new_root->content[0], new_root->right->content[0]);
+			new_root->right = build_tree(&current_token, current_type);	
 			if (!current_token->next)
 				return (new_root);
 			old_root = new_root;
 		}
 	}
-	//print_tree(new_root);
-	printf("end_of_construction! Actual returned root is : %s\n", new_root->content[0]);
 	return (new_root);
 }
 
--- srcs/exec.c
+++ srcs/exec.c
@@ -6,31 +6,53 @@
 /*   By: npolack <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 19:23:44 by npolack           #+#    #+#             */
-/*   Updated: 2025/01/17 16:25:01 by npolack          ###   ########.fr       */
+/*   Updated: 2025/01/20 18:27:25 by npolack          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	exec_CMD(t_bintree *leaf, char **envp)
+int	exec_cmd(t_bintree *leaf, char **envp)
 {
 	int pid;
 
 	pid = fork();
 	if (!pid)
 	{
-		execve
+		execve(leaf->content[0]
+}
 
+int	pipe_operation()
+{
+	int	pid;
+
+	pid = fork();
+	if (!pid)
+	
+}
+
+int	make_operation(t_bintree *root, char **envp)
+{
+	if (ft_strcmp(root->content[0], "|"))
+		pipe_operation();
+	else if (ft_strcmp(root->content[0], "||")
+		or_operation();
+	else if (ft_strcmp(root->content[0], "&&"))
+		and_operation();
+}
 
 int	execute_tree(t_bintree *root, char **envp)
 {
 	if (root->left)
-	{
-		if (root->type = PIPE
 		execute_tree(root->left);
+
+	if (root->type = OPERATOR);
+		make_operation(root);
+	else if (root->type = CMD)
+		exec_cmd(root);
+
+
 	if (root->right)
 		execute_tree(root->right);
-	if (root->type = OPERATOR)
-		make_operation(root, envp);
-	if (root->type = 
+
 }
