b4c7d05d3af65ec3e7985036eeaed1be476f0d9a
Jean-Matthieu Hervoche <jhervoch@3E2.42angouleme.fr>
2025-01-27 10:22:01 +0100

    Makefiletestunit

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: npolack <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/01/16 16:15:39 by npolack           #+#    #+#              #
-#    Updated: 2025/01/24 22:59:36 by ilia             ###   ########.fr        #
+#    Updated: 2025/01/27 10:12:35 by jhervoch         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -38,8 +38,7 @@ SRCS_DIR	= srcs
 OBJS_DIR	= objs
 C_FLAGS		= -Wall -Wextra -Werror -g
 
-SRCS		=	minishell.c\
-				binary_tree.c\
+SRCS		=	binary_tree.c\
 				tokenize.c\
 				token_tab.c\
 				token_utils.c\
@@ -52,6 +51,14 @@ SRCS		=	minishell.c\
 				signals.c\
 
 SRCS		:= $(addprefix $(SRCS_DIR)/, $(SRCS))
+
+MINISHELL_SRC := $(SRCS_DIR)/minishell.c
+MINISHELL_JM_SRC := $(SRCS_DIR)/minishell-jm.c
+MINISHELL_NIL_SRC := $(SRCS_DIR)/minishell-nil.c
+MINISHELL_OBJ := $(OBJS_DIR)/minishell.o
+MINISHELL_JM_OBJ := $(OBJS_DIR)/minishell-jm.o
+MINISHELL_NIL_OBJ := $(OBJS_DIR)/minishell-nil.o
+
 OBJS		= $(SRCS:$(SRCS_DIR)/%.c=$(OBJS_DIR)/%.o)
 
 msg				:
@@ -66,11 +73,33 @@ msg				:
 
 all				: $(NAME) 
 
+jm: fclean $(OBJS) $(MINISHELL_JM_OBJ) $(LIBFT)
+	@mkdir -p $(OBJS_DIR)
+	$(CC) $(C_FLAGS) $(INCLUDE) $^ -o $@
+	@echo "Compiled with minishell-jm.c"
+
+nil:fclean $(OBJS) $(MINISHELL_NIL_OBJ) $(LIBFT)
+	@mkdir -p $(OBJS_DIR)
+	$(CC) $(C_FLAGS) $(INCLUDE) $^ -o $@
+	@echo "Compiled with minishell-nil.c"
+
 $(OBJS_DIR)/%.o	: $(SRCS_DIR)/%.c 
 	@mkdir -p $(OBJS_DIR)
 	$(CC) $(C_FLAGS) $(INCLUDE) -c $< -o $@
 
-$(NAME)			:  $(OBJS) $(LIBFT)
+$(MINISHELL_OBJ) : $(MINISHELL_SRC)
+	@mkdir -p $(OBJS_DIR)
+	$(CC) $(C_FLAGS) $(INCLUDE) -c $< -o $@
+
+$(MINISHELL_JM_OBJ) : $(MINISHELL_JM_SRC)
+	@mkdir -p $(OBJS_DIR)
+	$(CC) $(C_FLAGS) $(INCLUDE) -c $< -o $@
+
+$(MINISHELL_NIL_OBJ) : $(MINISHELL_NIL_SRC)
+	@mkdir -p $(OBJS_DIR)
+	$(CC) $(C_FLAGS) $(INCLUDE) -c $< -o $@
+
+$(NAME)			:  $(OBJS) $(MINISHELL_OBJ) $(LIBFT)
 	@mkdir -p bin
 	$(CC) $(C_FLAGS) $(INCLUDE) $^ $(RLLIB) -o $@
 
@@ -78,11 +107,11 @@ $(LIBFT)		:
 	make -C libft
 
 clean			:
-	make clean -C libft
+	@make clean -C libft
 	rm -fr $(OBJS_DIR)
 
 fclean			: clean
-	make fclean -C libft
+	@make fclean -C libft
 	rm -fr bin
 
 re				: fclean all
---
+++ srcs/minishell-jm.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: npolack <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/16 16:19:01 by npolack           #+#    #+#             */
+/*   Updated: 2025/01/25 19:47:13 by npolack          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+// get user prompt
+char	*listen_to_user(char *prompt)
+{
+	char	*input;
+
+	input = readline(prompt);
+	if (!input)
+		return (NULL);
+	add_history(input);
+	return (input);
+}
+
+void	init_shell(t_data *data)
+{
+	rl_catch_signals = 0; // Disable readline's signal handling
+	signal_caught = 0;
+	data->tree = NULL;
+	data->token_list = NULL;
+	data->paths = NULL;
+	data->user_input = NULL;
+}
+
+int	run_shell(t_data *data)
+{
+	t_token		*cpy;
+	char		wd[1024];
+	char		*tmp;
+
+
+	while (1)
+	{
+		if (getcwd(wd, sizeof(wd)))
+		{
+			tmp = ft_strjoin("M!N!$H3LL>", wd);
+			data->prompt = ft_strjoin(tmp, ">$");
+			free(tmp);
+		}
+		data->user_input = listen_to_user(data->prompt);
+		if (!data->user_input || !ft_strcmp(data->user_input, ""))	// doesnt work either...
+		{
+			free(data->user_input);
+			free(data->prompt);
+			ft_putendl_fd("exit", 2);
+			exit(0);
+		}
+		printf("\n\n----------- List of token is : -");
+		data->token_list = tokenize(data->user_input);
+		print_list(data->token_list);
+		printf("\n\n----------- DEBUG TREE ---------");
+		cpy = data->token_list;
+		data->tree = build_tree(&cpy, CMD);
+		print_tree(data->tree, 0); // print the tree for debug
+		printf("\n\n----------- EXECUTION ----------");
+		execute_tree(data); 
+		free_minishell(data);
+		init_shell(data);
+	}
+}
+
+int	main(int ac, char **argv, char **envp)
+{
+	t_data				data;
+
+	(void)argv;
+	if (ac > 1)
+		printf("minishell: ???\n");
+	register_signals();
+	data.envp = envp;
+	init_shell(&data);
+	run_shell(&data);
+	free_minishell(&data);
+	return (signal_caught);
+}
