935ff1cec289311a60bfb5f7becd29a2a9ee19ef
Nil Polack <npolack@2C5.42angouleme.fr>
2025-02-26 21:16:49 +0100

    solve binary_tree issues

--- srcs/tokenize/tokenize.c
+++ srcs/tokenize/tokenize.c
@@ -6,7 +6,7 @@
 /*   By: npolack <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 00:21:43 by npolack           #+#    #+#             */
-/*   Updated: 2025/02/26 18:19:46 by npolack          ###   ########.fr       */
+/*   Updated: 2025/02/26 19:33:28 by npolack          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,8 @@ static t_token	*build_tokenlist(char **tokens)
 		if (!curr_token)
 			return (NULL);
 	}
-	if (curr_token && curr_token->type != CMD)
+	if (curr_token && curr_token->type != CMD
+		&& ft_strcmp(curr_token->input, ")"))
 	{
 		ft_lstclear_token(&head, &free);
 		ft_printf(2, SYNTERR);
